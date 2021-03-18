f2d45b1fce7cf0a10c438c5f793ad15b4c7778b3
mmayes098 <mayes.matthewj@gmail.com>
2021-03-18 16:45:55 -0400

    Changed JS comments

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ window.onload = grid(16);
 //set up a hover effect so the grid divs change color when your mouse passes over them
     //set up event listeners for when mouse enters a div and when mouse leaves a div
     //ways to change color of divs: add new class to div, change div's background color using JS (most likely)
-
+    //The below code works...but when you mouse into the first column it changes the whole container, and when you mouse out of the last column it changes the whole container
 
 const wrapper = document.getElementById('main');
 
@@ -41,26 +41,6 @@ wrapper.addEventListener('mouseover', function(e) {
     let element = document.elementFromPoint(e.clientX, e.clientY)
     element.style.backgroundColor = randomHsl();
 });
-
-// var boxes = document.getElementsByClassName('box');
-// for (var i = 1, len = Math.sqrt(boxes.length); i <= len; i++) {
-//     for (var r = 1, len = Math.sqrt(boxes.length); r <= len; r++) {
-//         // console.log('box' + i + '-' + r);
-//         var box = document.getElementById('box' + i + '-' + r);
-//         box.addEventListener("mouseover", function() {
-//             box.style.backgroundColor = randomHsl();
-//         });
-//     };
-// };
-
-// console.log(box);
-
-// box.addEventListener("mouseover", function() {
-//     box.style.backgroundColor = randomHsl();
-// });
-
-// const $boxes = document.querySelectorAll('.box');
-// $boxes.forEach($box => $box.onmouseover = changeBackground($box));
 
 //add a button to the top of the screen which will clear current grid and send user a popup asking for how many squares per side to make a new grid (no more than 100 (or 64))
 //new grid gets generated in the same total space as the old grid
