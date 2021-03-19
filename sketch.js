45d7dc85e521b5f4501622abe5d37e47127db1d2
mmayes098 <mayes.matthewj@gmail.com>
2021-03-18 23:11:03 -0400

    Final product

--- sketch.js
+++ sketch.js
@@ -1,11 +1,3 @@
-//default upon opening is to create a grid with a 16x16 grid of square divs
-    //need to establish outer grid size and make cells fit inside that
-    //individual rows instead of whole grid?
-    //Hints from Odin:
-        //use JS to create grid
-        //ways to make divs appear as grid: float/clear, inline-block, flexbox, CSS Grid
-        //be careful with borders/margins, can adjust size of squares unintentionally
-
 function grid(num) {
     var container = document.createElement("div");
     container.id = "main";
@@ -26,39 +18,29 @@ function grid(num) {
             currentRow.appendChild(box);
         }
     }
+
+    container.addEventListener('mouseover', function(e) {
+        let element = document.elementFromPoint(e.clientX, e.clientY)
+        element.style.backgroundColor = randomHsl();
+    });
 }
 
 window.onload = grid(16);
 
-//set up a hover effect so the grid divs change color when your mouse passes over them
-    //set up event listeners for when mouse enters a div and when mouse leaves a div
-    //ways to change color of divs: add new class to div, change div's background color using JS (most likely)
-    //The below code works...but when you mouse into the first column it changes the whole container, and when you mouse out of the last column it changes the whole container
+function resetGrid() {
+    var container = document.getElementById('main')
+    container.remove();
+    grid(16);
+}
 
-const wrapper = document.getElementById('main');
-
-wrapper.addEventListener('mouseover', function(e) {
-    let element = document.elementFromPoint(e.clientX, e.clientY)
-    element.style.backgroundColor = randomHsl();
-});
-
-//add a button to the top of the screen which will clear current grid and send user a popup asking for how many squares per side to make a new grid (no more than 100 (or 64))
-//new grid gets generated in the same total space as the old grid
-    //Hints from Odin:
-        //research button tags in HTML and how you can make a JS function run when one is clicked
-            //write JS function to change grid size
-        //Also check out prompt (JS)
-        //Should be able to type 64 into the prompt and it will create a brand new 64x64 grid without changing the total amount of pixels used
-
-//Optional (but I want to do it): Instead of just changing the color from white to black, have each pass through with the mouse change to a completely random RGB value. Or have it add 10% of black on each pass.
-
-//Push to GitHub!
-
-
-
-// cell.addEventListener("mouseout", function() {
-//     cell.style.backgroundColor = "white";
-// });
+function newGrid() {
+    var container = document.getElementById('main');
+    var num = prompt("Please enter a number between 1 and 40 for the length of the grid:", "16");
+    if (num != null) {
+        container.remove();
+        grid(num);
+    };
+}
 
 function changeBackground(box) {
     box.style.backgroundColor = randomHsl();
